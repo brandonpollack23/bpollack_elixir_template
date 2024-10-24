541e6811be486e66c9ec6004dbb4de550845ed96
Brandon Pollack <brandonpollack23@gmail.com>
2024-10-25 07:30:16 +0900

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# BpollackElixirTemplate
+> TODO: Add description**
+
+
+## Installation
+
+```elixir
+@deps [
+bpollack_elixir_template: "~> 0.1.0"
+]
+```
+
+## License
+
+> TODO: Add license
+
+----
+Created:  2024-10-24Z
---
+++ lib/bpollack_elixir_template.ex
@@ -0,0 +1,11 @@
+defmodule BpollackElixirTemplate do
+
+  @moduledoc File.read!(Path.join([__DIR__, "../README.md"]))
+  
+  use MixTemplates,
+    name:       :bpollack_elixir_template,
+    short_desc: "Template for ....",
+    source_dir: "../template"
+
+  
+end
